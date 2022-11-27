802364af8377af4f29f90e7f7984981ec5e0325c
Wasim Fatair <wasim.fatair@gmail.com>
2022-11-27 16:26:39 +0200

    Creating Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HttpRequestsHelloWorld';
+
+  AddPostForm: FormGroup;
+
+  ngOnInit(): void {
+    this.AddPostForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      content: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.AddPostForm.value);
+  }
 }
