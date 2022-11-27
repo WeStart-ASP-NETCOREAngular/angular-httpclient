334f2077dfd613ffd3e46ebb6a1ad4b91d71a362
Wasim Fatair <wasim.fatair@gmail.com>
2022-11-27 16:56:59 +0200

    HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'HttpRequestsHelloWorld';
-
+  baseUrl = environment.baseURL + '/api/posts';
+  constructor(private httpClient: HttpClient) {}
+  isFetching = false;
+  loadedPosts: { id: number; title: string; content: string }[] = [];
   AddPostForm: FormGroup;
 
   ngOnInit(): void {
+    this.fetchPosts();
     this.AddPostForm = new FormGroup({
       title: new FormControl('', [Validators.required]),
       content: new FormControl('', Validators.required),
@@ -20,5 +26,38 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log(this.AddPostForm.value);
+    // this.httpClient.post('https://localhost:7254/api/posts', {});
+    this.httpClient
+      .post(this.baseUrl, this.AddPostForm.value)
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.httpClient
+      .get<{ id: number; title: string; content: string }[]>(this.baseUrl)
+      .subscribe({
+        next: (data) => {
+          this.loadedPosts = data;
+          this.isFetching = false;
+        },
+        error: (error) => {
+          this.isFetching = false;
+        },
+      });
+  }
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onDeletePost(postId: number) {
+    this.httpClient
+      .delete(`${this.baseUrl}/${postId}`)
+      .subscribe((response) => {
+        console.log(response);
+        this.fetchPosts();
+      });
   }
 }
